3cff4a15719eb30b53298e11346eb06bfa18aeff
Alexander Standke <xanderstrike@gmail.com>
2017-09-14 20:49:09 -0700

    add user auth and first time setup

--- server.py
+++ server.py
@@ -1,21 +1,51 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__, static_url_path='/static/')
+import os, bcrypt
+
+app = Flask(__name__)
+
+salt = ''
+if os.path.isfile("data/salt"):
+  salt = open("data/salt", "r").read()
+else:
+  salt = bcrypt.gensalt()
+  open("data/salt", "w").write(salt)
 
 @app.route("/")
 def index():
-  return app.send_static_file('index.html')
+  if os.path.isfile("data/index.html"):
+    return open("data/index.html", "r").read()
+  else:
+    return redirect("/setup")
 
 @app.route("/edit")
 def edit():
-  f = open("static/index.html", "r")
-  return render_template('edit.html', content=f.read())
+  if os.path.isfile("data/index.html"):
+    f = open("data/index.html", "r")
+    return render_template("edit.html", content=f.read())
+  else:
+    return redirect("/setup")
+
+@app.route("/setup")
+def setup():
+  if os.path.isfile("data/index.html"):
+    return redirect("/")
+  else:
+    return render_template("setup.html")
 
 @app.route("/update")
 def update():
-  content = request.args.get('content')
-  f = open("static/index.html", "w")
+  if os.path.isfile("data/password") and open("data/password", "r").read() != bcrypt.hashpw(request.args.get("password").encode('utf-8'), salt):
+    return redirect("/")
+
+  newpass = request.args.get("newpassword")
+  if newpass and newpass != '':
+    hashed = bcrypt.hashpw(request.args.get("newpassword").encode('utf-8'), salt)
+    open("data/password", "w").write(hashed)
+
+  content = request.args.get("content")
+  f = open("data/index.html", "w")
   f.write(content)
-  return redirect('/')
+  return redirect("/")
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=4545)
+  app.run(host="0.0.0.0", port=4545)
